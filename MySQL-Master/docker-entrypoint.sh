e50d9d47ae2c967623e1471fef2f5caf86b918f3
vahidsamie <vahid.samie@gmail.com>
2023-08-22 14:04:34 +0330

    commit

---
+++ MySQL-Master/docker-entrypoint.sh
@@ -0,0 +1,25 @@
+#!/bin/bash
+set -e
+
+# ... (existing script content)
+
+# Add your SQL commands here
+if [ "$1" = 'mysqld' ]; then
+    mysql -uroot -p"$MYSQL_ROOT_PASSWORD" <<EOF
+        GRANT CREATE, CREATE USER ON *.* TO 'vahid'@'%';
+        CREATE USER 'replication'@'%' IDENTIFIED BY 'SQ1MasTer1092';
+        GRANT REPLICATION SLAVE ON *.* TO 'replication'@'%';
+        CREATE DATABASE UserInfo;
+        USE UserInfo;
+        CREATE TABLE users (
+            id INT AUTO_INCREMENT PRIMARY KEY,
+            name VARCHAR(50),
+            age INT,
+            phone VARCHAR(15),
+            location VARCHAR(100),
+            gender VARCHAR(10)
+        );
+    EOF
+
+    exec "$@"
+fi
